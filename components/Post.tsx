327a81d83ea7b62a445e38acd55347e68ab4f556
Victor Vila Ases <irikuo@gmail.com>
2020-03-31 21:35:09 +0200

    Upgraded dependencies

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NextPage } from 'next';
-import Head from 'next/head';
 import Link from 'next/link';
 import format from 'date-fns/format';
 import Layout from './Layout';
